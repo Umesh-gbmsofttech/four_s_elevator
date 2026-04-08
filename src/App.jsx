48dcde9d9b889769d448a7ddefc13b0f02586f99
Shriram111 <shriramsgunjal@gmail.com>
2026-04-08 21:43:09 +0530

    ScrollTop Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Navbar } from './sections/Navbar';
 import Footer from './sections/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ServicesPage from './pages/ServicesPage';
 import ProductsPage from './pages/ProductsPage';
+import ScrollToTopButton from './components/ScrollToTop'; // The floating button
+
+// Helper component to reset scroll position on page change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* Resets scroll position when URL changes */}
       <Navbar />
+      <ScrollToTopButton /> {/* Floating button to scroll up */}
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/products" element={<ProductsPage />} />
       </Routes>
+      
       <Footer />
     </Router>
   );
 }
 
-// THIS LINE IS WHAT main.jsx IS LOOKING FOR:
 export default App;
